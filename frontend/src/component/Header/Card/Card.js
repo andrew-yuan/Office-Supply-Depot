870f0edaf1e21be3e1ed4d4fff0e1169d4d3105d
andrew-yuan <andrewyuan1996@gmail.com>
2019-05-01 07:24:43 -0700

    5/1/19 OSD project

---
+++ frontend/src/component/Header/Card/Card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Card.scss'
+import { withRouter } from "react-router";
+
+const goToCarddetails = (event,props) => {
+
+
+    props.history.push('/productdetail/'+ props.cardId);
+    event.preventDefault();
+}
+const Card = (props) => {
+
+
+
+    return (
+        <div onClick = {(event)=>goToCarddetails(event,props)} className='cardContent'>
+            <img className="rounded picsize" src= {props.cardUrl} />
+            <p className='short-description'>{props.cardName}</p>
+            <p className='price'>${props.cardPrice}</p>
+        </div>
+    );
+};
+
+export default withRouter(Card);
